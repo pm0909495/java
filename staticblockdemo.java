fbc4615968355dc24e06dbebdc9c1bf914c8a271
keerthana.pillagola <151884155+pm0909495@users.noreply.github.com>
2024-08-22 11:45:37 +0530

    Create staticblockdemo.java

---
+++ staticblockdemo.java
@@ -0,0 +1,9 @@
+import java.io.*;
+class staticblockdemo{
+  public static void main(String args[]){
+    System.out.println("Inside the main() method");
+    }
+    static{
+      System.out.println("Inside static block");
+    }
+}

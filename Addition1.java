c8b132c231707ebb0fb07e8bd187c4549160d4e4
keerthana.pillagolla <151884155+pm0909495@users.noreply.github.com>
2024-09-19 10:31:45 +0530

    Create Addition1.java

---
+++ Addition1.java
@@ -0,0 +1,9 @@
+import java.io.*;
+public class Addition1 extends Object{
+  public static void main(String args[]){
+    int x=-5;
+    int y=10;
+    int z=x+y;
+    System.out.println(" sum of " +x+ " and " +y+ " is " +z);
+  }
+}
